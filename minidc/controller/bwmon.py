7f4f1e5792475cabd0a9a663b8390c67b93026d1
Tran Ly Vu <vutransingapore@gmail.com>
2018-01-09 14:22:57 +0800

    update stat reply in controller
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- minidc/controller/bwmon.py
+++ minidc/controller/bwmon.py
@@ -143,10 +143,10 @@ class BandwidthMonitor(app_manager.RyuApp):
 
         # [ ADD YOUR CODE HERE ]
         for stat in body:
-            transmitted_bytes = 
-            received_bytes =
-
-            self.bwstats.addHostBwStat(name, transmitted_bytes, received_bytes)
+            if stat.port_no in self.topo.ports[name].values:
+                transmitted_bytes = stat.tx_bytes
+                received_bytes = stat.rx_bytes
+        self.bwstats.addHostBwStat(name, transmitted_bytes, received_bytes)
 
 
         # periodically print tenant bandwidth usage
