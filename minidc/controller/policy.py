ab8cc29ee8d0c5288a83ddda5bb06d12da45b157
Tran Ly Vu <vutransingapore@gmail.com>
2018-01-10 11:04:29 +0800

    update policy
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- minidc/controller/policy.py
+++ minidc/controller/policy.py
@@ -172,6 +172,9 @@ class StaticPolicy(object):
         #           topo.ports[edge switch name][host name]
         #   (Hint: to find a the VLAN, use topo.getVlanCore(vlanId))
         # create rules for packets from edge -> core (upward)
+        core_vlan0 = topo.coreSwitches.keys()[0]
+        core_vlan1 = topo.coreSwitches.keys()[1]
+        
         for edge in topo.edgeSwitches.values():
             routingTable[edge.dpid] = []
             for h in topo.hosts.values():
