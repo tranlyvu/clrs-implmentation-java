95c435d788b3edeef5e3d4f22e86fb480de10bfd
Tran Ly Vu <vutransingapore@gmail.com>
2018-01-10 10:47:17 +0800

    update policy
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- minidc/controller/policy.py
+++ minidc/controller/policy.py
@@ -171,7 +171,28 @@ class StaticPolicy(object):
         #   (Hint: you can look up the port of a neighboring host using
         #           topo.ports[edge switch name][host name]
         #   (Hint: to find a the VLAN, use topo.getVlanCore(vlanId))
-        for edge in top.edgeSwitches.values():
+        # create rules for packets from edge -> core (upward)
+        core_vlan0 = topo.coreSwitches.keys()[0]
+        core_vlan1 = topo.coreSwitches.keys()[1]
+
+        for edge in topo.edgeSwitches.values():
+            routingTable[edge.dpid] = []
+            for h in topo.hosts.values():
+                # output the appropriate port if the destination is a neighboring host 
+                if h.name in edge.neighbors:
+                    output = topo.ports[edge.name][h.name]
+                else
+                    if 's101' is edge.name:
+                        output = topo.ports[edge.name][core_vlan1]
+                    else:
+                        output = topo.ports[edge.name][core_vlan0]
+
+                routingTable[edge.dpi].append({
+                    'eth_dst' : h.eth,
+                    'output' : [output],
+                    'priority' : 2,
+                    'type' : 'dst'
+                })
             
 
 
